00e4c601ae245282237b398671e5d0409635f3c8
Allen-Pinto <pinto.allen05@gmail.com>
2025-02-14 18:05:51 +0530

    Update verifyUser.js

--- backend/utils/verifyUser.js
+++ backend/utils/verifyUser.js
@@ -1,19 +1,24 @@
-import { errorHandler } from "./error.js"
-import jwt from "jsonwebtoken"
+import { errorHandler } from "./error.js";
+import jwt from 'jsonwebtoken';
 
 export const verifyToken = (req, res, next) => {
-  const token = req.cookies.access_token
+  // Check for token in cookies or Authorization header
+  const token = req.cookies.access_token || req.headers['authorization']?.split(' ')[1];
 
+  // If no token found, return 401 Unauthorized error
   if (!token) {
-    return next(errorHandler(401, "Unauthorized"))
+    return next(errorHandler(401, "No token provided"));
   }
 
+  // Verify the token
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) {
-      return next(errorHandler(403, "Forbidden"))
+      // If token verification fails, return 403 Forbidden error
+      return next(errorHandler(403, "Invalid token"));
     }
 
-    req.user = user
-    next()
-  })
-}
+    // Attach user data to the request object
+    req.user = user;
+    next();
+  });
+};
