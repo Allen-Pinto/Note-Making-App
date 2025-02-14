f1fd940da6b11e287b1412d2c4b482983ce7e1da
Allen-Pinto <pinto.allen05@gmail.com>
2025-02-14 14:16:13 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,15 @@ const app = express();
 // to make input as json
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: ["https://note-making-app-sigma.vercel.app/"], credentials: true }));
+app.use(
+  cors({
+    origin: "https://note-making-app-sigma.vercel.app",
+    methods: "GET,POST,PUT,DELETE,PATCH,OPTIONS",
+    allowedHeaders: "Content-Type,Authorization",
+    credentials: true,
+  })
+);
+
 
 // import routes
 import authRouter from "./routes/auth.route.js";
