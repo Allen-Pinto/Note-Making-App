74e28cacc6c941afb55577fd34d157682743b983
Allen-Pinto <pinto.allen05@gmail.com>
2025-02-20 11:53:35 +0530

    Update auth.route.js

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,14 +1,10 @@
 import express from "express"
 import { signin, signout, signup } from "../controller/auth.controller.js"
-import { verifyToken } from "../utils/verifyUser.js"
 
 const router = express.Router()
 
 router.post("/signup", signup)
 router.post("/signin", signin)
-router.post("/signout", (req, res) => {
-  res.clearCookie("access_token", { httpOnly: true, secure: true, sameSite: "None" })
-  return res.status(200).json({ success: true, message: "Logged out successfully" })
-})
+router.post("/signout", signout) // Use the controller function here
 
 export default router
